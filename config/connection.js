91e40632524fa97802b0c89064c46bd2b689bd71
StevenDong2512 <stevendong1997@outlook.com>
2023-06-19 17:55:41 +1000

    connection

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONOGDB_URI || "mongodb://localhost:27017/StevenDong",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
